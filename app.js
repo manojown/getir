45cec47095ada1f3fe63d3c96f9046975eb1ed4a
manoj choudhary <manojchoudhary@manojs-MacBook-Pro.local>
2021-09-02 14:15:14 +0530

    remove commented code

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const { handleError, ErrorHandler } = require('./helpers/custom-error');
-var recordRouter = require('./routes/record');
+const recordRouter = require('./routes/record');
 require('dotenv').config()
-var app = express();
+const app = express();
 
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/record', recordRouter);
 
@@ -25,18 +20,9 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+// other unhandled errors
 app.use((err, req, res, next) => {
   handleError(err, res);
 });
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 module.exports = app;
